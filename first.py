50c46c317fb5bfbd8655565c82c1f0f9a6bfcc1f
ankitgujre <kumarankit65494@gmail.com>
2025-03-26 20:47:50 +0530

    Your commit message

---
+++ first.py
@@ -0,0 +1,8 @@
+print("First program")
+print(20+56)
+print('20+56')
+
+print("Hello World", "this is my first program", 1)
+
+print(10>1)
+print(5<1)
---
+++ varcheck.py
@@ -0,0 +1,12 @@
+a = 10
+b = 10
+print(a, b)
+print(type(a))
+
+print(id(a), id(b))  #for checking memory address using id() function
+
+a = "ankit"
+b = "gujre"
+c = 20
+print(a+" "+b)
+print(c+30)
